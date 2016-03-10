94541e8e0949bafd0b9612a4385c090d027762e1
wuxinbo <530286373@qq.com>
2016-03-10 11:13:36 +0800

    新增测试文件下载方法

--- src/com/wu/test/FileUtilTest.java
+++ src/com/wu/test/FileUtilTest.java
@@ -1,12 +1,9 @@
 package com.wu.test;
 
-import static org.junit.Assert.*;
-
-import java.io.IOException;
-
+import com.wu.util.FileUtil;
 import org.junit.Test;
 
-import com.wu.util.FileUtil;
+import java.io.IOException;
 
 public class FileUtilTest {
 
@@ -14,5 +11,12 @@ public class FileUtilTest {
 	public void test() {
 		System.out.println(Math.pow(2, 3));
 	}
-
+	@org.testng.annotations.Test
+	public void testDownload(){
+		try {
+			FileUtil.DownloadFile("http://101.231.114.233:8085/TSM_DEV//resources/images/04_03100000_20140813092443.png","/home/wuxinbo/bank/");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
